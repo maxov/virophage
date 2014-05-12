0864246540257d0f155d99766408dcfb6e599fa6
leontren <leontren@gmail.com>
2014-05-12 15:33:14 -0700

    hi max

--- src/main/java/virophage/game/Game.java
+++ src/main/java/virophage/game/Game.java
@@ -3,14 +3,6 @@ package virophage.game;
 public class Game {
 
     public void nun() {
-        EventDispatcher e = new EventDispatcher();
-        e.register("abc", new EventListener<Object>() {
-            @Override
-            public void onEvent(Object evt) {
-                EventDispatcher.X e = (EventDispatcher.X) evt;
-                e.v();
-            }
-        });
     }
 
 }
