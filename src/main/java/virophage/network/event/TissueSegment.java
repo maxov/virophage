0a2cb9c46787a9df30d96d0a52f619b54648a948
gratimax <max@ovsankin.com>
2014-05-20 08:03:40 -0700

    merged, network stuff

---
+++ src/main/java/virophage/network/event/TissueSegment.java
@@ -0,0 +1,28 @@
+package virophage.network.event;
+
+import virophage.core.*;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class TissueSegment implements Serializable {
+
+    private Cell[] cells;
+    private Channel[] channels;
+
+    public TissueSegment(Tissue tissue, ArrayList<Location> locations) {
+        cells = new Cell[locations.size()];
+
+        for(int i = 0; i < locations.size(); i++) {
+            cells[i] = tissue.getCell(locations.get(i));
+        }
+
+        for(Player player: tissue.getPlayers()) {
+            for(Channel channel: player.channels) {
+
+            }
+        }
+
+    }
+
+}
