9860ebd90e7ea9e3e88fc73ded6ce5d763e6e33c
leontren <leontren@gmail.com>
2014-05-19 22:31:47 -0700

    Machine Player added (for use in Stupid AI). GameScreen changed to InstructionScreen.

---
+++ src/main/java/virophage/core/MachinePlayer.java
@@ -0,0 +1,12 @@
+package virophage.core;
+
+import java.awt.Color;
+
+public class MachinePlayer extends Player{
+
+	public MachinePlayer(Color color, Tissue tissue) {
+		super(color, tissue);
+		// TODO Auto-generated constructor stub
+	}
+
+}
